66e96ce378c1837d2b34a75dc47f6ad7d4310098
psantacreu <88888097+psantacreu@users.noreply.github.com>
2023-08-11 16:32:06 +0200

    important changes web

--- web.py
+++ web.py
@@ -5,25 +5,7 @@ from streamlit_lottie import st_lottie
 
 st.set_page_config(page_title="Pau Santacreu", page_icon="👤", layout="centered")
 
-# ---- LOAD ASSETS ----
-lottie_coding = "https://lottie.host/56477275-3dc5-4903-aec6-7b2bdced418c/7zVciE3bcb.json" #animation
-img_drink_vermu = Image.open("/Users/pau/Desktop/programing/images/Frame.png")
-img_art = Image.open("/Users/pau/Desktop/programing/images/art.png")
-
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-# ---- Use Local CSS ----
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-local_css("/Users/pau/Desktop/programing/style/style.css")
-
-# ---- background color ----
+# ---- CSS ----
 background_color = "#fbfaf6"
 css = f"""
     <style>
@@ -35,8 +17,7 @@ css = f"""
 """
 st.markdown(css, unsafe_allow_html=True)
 
-
-# ---- Content ----
+# ---- Page content ----
 with st.container():
     st.subheader("hey I'm Pau Santacreu :wave:")
     st.write("##")
